f453b576090b9ea92e96fb584c84bf53e847e697
tomoya yoshida <ihatov08@gmail.com>
2025-05-23 14:18:57 +0900

    bin/importmap pin toastr

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "marked" # @15.0.12
 pin "dompurify" # @3.2.6
+pin "toastr" # @2.1.4
+pin "jquery" # @3.7.1
