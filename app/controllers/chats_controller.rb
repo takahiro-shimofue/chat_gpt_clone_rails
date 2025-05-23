823422e61ce33b3cc7e1d8a80e71d94de88e9710
tomoya yoshida <ihatov08@gmail.com>
2025-05-23 10:34:58 +0900

    Chatをするcontroller作成

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,4 +1,43 @@
 class ChatsController < ApplicationController
+  include ActionController::Live
+
   def index
+  end
+
+  def create
+    set_streaming_headers
+    sse = SSE.new(response.stream, event: "message")
+    client = OpenAI::Client.new(
+      access_token: Rails.application.credentials.dig(:gemini, :api_key),
+      uri_base: "https://generativelanguage.googleapis.com/v1beta/openai/"
+    )
+
+    begin
+      stream_chat_response(client, sse)
+    ensure
+      sse.close
+    end
+  end
+
+  private
+
+  def set_streaming_headers
+    response.headers["Content-Type"]  = "text/event-stream"
+    response.headers["Last-Modified"] = Time.now.httpdate
+  end
+
+  def stream_chat_response(client, sse)
+    client.chat(
+      parameters: {
+        model: "gemini-2.0-flash",
+        messages: [ { role: "user", content: params[:prompt] } ],
+        stream: proc do |chunk|
+          content = chunk.dig("choices", 0, "delta", "content")
+          puts "content: #{content}"
+          puts "chunk: #{chunk}"
+          sse.write({ message: content }) if content
+        end
+      }
+    )
   end
 end
