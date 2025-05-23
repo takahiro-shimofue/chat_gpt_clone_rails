8ba56868abf9bb1fe609ee52af8fada09baccf08
tomoya yoshida <ihatov08@gmail.com>
2025-05-23 16:36:36 +0900

    ドロップダウンを動作させる

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,6 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dropdown"
 export default class extends Controller {
-  connect() {
+  static targets = ["menu"]
+
+  toggle() {
+    console.log(this.menuTarget.classList)
+    if (this.menuTarget.classList.contains("hidden")) {
+      this.menuTarget.classList.remove("hidden")
+    } else {
+      this.menuTarget.classList.add("hidden")
+    }
   }
 }
