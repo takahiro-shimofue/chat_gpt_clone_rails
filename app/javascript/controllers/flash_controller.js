929f220b4c6611ce40e5f8657cfe5696da07fe65
tomoya yoshida <ihatov08@gmail.com>
2025-05-23 14:55:10 +0900

    Flashメッセージを表示

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,7 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
+import toastr from "toastr"
 
 // Connects to data-controller="flash"
 export default class extends Controller {
   connect() {
+    console.log("Flash controller connected")
+    const flashType = this.element.dataset.flashType
+    const flashMessage = this.element.dataset.flashMessage
+
+    if (flashType === "notice") {
+      toastr.success(flashMessage, "Success")
+    }
+    if (flashType === "alert") {
+      toastr.error(flashMessage, "Error")
+    }
   }
 }
