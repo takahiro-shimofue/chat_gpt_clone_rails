28ab2c2044116c5d1a41ceb3a3497ced051286c3
tomoya yoshida <ihatov08@gmail.com>
2025-05-23 20:41:16 +0900

    uuidのvalidationを追加

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -3,4 +3,16 @@ class Chat < ApplicationRecord
   has_many :messages, dependent: :destroy
 
   validates :uuid, presence: true, uniqueness: true
+
+  validate :uuid_format
+
+  private
+
+  UUID_REGEXP = /\A[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\z/i
+
+  def uuid_format
+    unless uuid =~ UUID_REGEXP
+      errors.add(:uuid, "must be a valid UUID format")
+    end
+  end
 end
