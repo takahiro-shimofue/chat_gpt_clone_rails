757eacea4843ba35d2721687b061603d3742eb66
tomoya yoshida <ihatov08@gmail.com>
2025-05-23 20:42:46 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resource :session
   resources :passwords, param: :token
   root "chats#index"
-  resources :chats, only: [ :index, :create ]
+  resources :chats, only: [ :index, :create, :show ], param: :uuid
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
